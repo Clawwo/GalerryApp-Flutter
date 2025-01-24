530d846ccd89feeaa70215b4cc9cf670f1ab451a
Clawwo <genzz760@gmail.com>
2025-01-25 06:53:25 +0700

    Add all project files

---
+++ android/app/src/main/kotlin/com/example/galeri_gambar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.galeri_gambar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gallery App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const GalleryScreen(),
+    );
+  }
+}
+
+class GalleryScreen extends StatelessWidget {
+  const GalleryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Daftar gambar
+    final List<String> images = [
+      'assets/images/gunung.jpg',
+      'assets/images/gunung.jpg',
+      'assets/images/gunung.jpg',
+      'assets/images/gunung.jpg',
+      'assets/images/gunung.jpg',
+      'assets/images/gunung.jpg',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gallery App'),
+        centerTitle: true,
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // Jumlah kolom dalam grid
+          crossAxisSpacing: 8.0, // Jarak antar kolom
+          mainAxisSpacing: 8.0, // Jarak antar baris
+        ),
+        padding: const EdgeInsets.all(8.0),
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              // Navigasi ke detail gambar
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ImageDetailScreen(
+                    imagePath: images[index],
+                  ),
+                ),
+              );
+            },
+            child: Image.asset(
+              images[index],
+              fit: BoxFit.cover, // Atur gambar agar pas di grid
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ImageDetailScreen extends StatelessWidget {
+  final String imagePath;
+
+  const ImageDetailScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Detail'),
+      ),
+      body: Center(
+        child: Image.asset(imagePath),
+      ),
+    );
+  }
+}
